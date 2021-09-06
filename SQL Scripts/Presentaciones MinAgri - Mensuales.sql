4958d7e4fb711d6f3817ba16c15842d476dc2cd5
Tomás A. Cardoner <tomas@cardoner.com.ar>
2021-09-06 18:36:04 -0300

    Update Presentaciones MinAgri - Mensuales.sql

--- SQL Scripts/Presentaciones MinAgri - Mensuales.sql
+++ SQL Scripts/Presentaciones MinAgri - Mensuales.sql
@@ -1,5 +1,5 @@
 USE CSMovimientoGranos
 GO
 
-EXEC dbo.usp_Movimiento_Cereal_MC14 1, 1, 2021, 6
-EXEC dbo.usp_Movimiento_SubProducto_MC15 1, 2, 2021, 6
+EXEC dbo.usp_Movimiento_Cereal_MC14 1, 1, 2021, 7
+EXEC dbo.usp_Movimiento_SubProducto_MC15 1, 2, 2021, 7
